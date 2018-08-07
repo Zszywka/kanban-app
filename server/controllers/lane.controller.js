21c5e944b9007122097e65a8dc1992c0c1b0f2b9
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-08-07 17:29:04 +0200

    Add first endpoint

--- server/controllers/lane.controller.js
+++ server/controllers/lane.controller.js
@@ -1,5 +1,26 @@
+//obsluga zapytania do endpointów
+//przetwarzanie zapytania i wykonanie odpowiednich modyfikacji na kolekcjach danych
+//tutaj: komunikację z naszą bazą MongoDB
 import Lane from '../models/lane';
-
-export function getSomething(req, res) {
-  return res.status(200).end();
+import uuid from 'uuid';
+//addLane sprawdza, czy w ciele zapytania znajduje się parametr name, w którym określamy nazwę linii
+export function addLane(req, res) {
+  //jeśli nie, odsyła status odp 403-serwer odmawia nawiązania połączenia
+  if (!req.body.name) {
+    res.status(403).end();
+  }
+//W przeciwnym wypadku tworzymy nowy model linii na podstawie ciała zapytania.
+  const newLane = new Lane(req.body);
+// Do nowo utworzonego modelu dodajemy pustą tablicę
+  newLane.notes = [];
+//unikalny id
+  newLane.id = uuid();
+//zapisać model do bazy, obsługując przy tym odpowiednio błędy
+  newLane.save((err, saved) => {
+    if (err) {
+    //500-blad servera
+      res.status(500).send(err);
+    }
+    res.json(saved);
+  });
 }
--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -1,7 +1,9 @@
+//unkcja dodawania linii do routingu
 import { Router } from 'express';
 import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
-
+// Add a new Lane
+router.route('/lanes').post(LaneController.addLane);
 
 export default router;
