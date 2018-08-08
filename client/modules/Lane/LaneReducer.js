b2a7f7aebfc80b08ddb894aa92eea215118f36ab
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-08-08 03:26:06 +0200

    Add reducer implementation

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,5 +1,6 @@
 // Import Actions
 import { CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './LaneActions';
+import { CREATE_NOTE, DELETE_NOTE } from '../Note/NoteActions';
 
 // Initial State
 const initialState = {};
@@ -26,6 +27,31 @@ export default function lanes(state = initialState, action) {
 //usiniecie calej lini
     case DELETE_LANE:
       return state.filter(lane => lane.id !== action.laneId);
+//dodanie notatki
+    case CREATE_NOTE:
+    //mapujemy nasz stan (czyli tablicę linii)
+      return state.map(lane => {
+//jeśli id naszej linii to ten, który przekazaliśmy w akcji,to znaczy,
+//że do niej właśnie chcemy dodać notkę,
+        if (lane.id === action.laneId) {
+//za pomocą spread operatora tworzymy nową tablicę identyfikatorów notek z dodanym id nowej notki
+          const notes = [...lane.notes, action.note.id];
+          //zwracamy nową linię również korzystając ze spreada dla obiektów,
+          return { ...lane, notes };
+        }
+//jeśli akcja nie dotyczy tej linii, to niczego z nią nie robimy — zwracamy taką, jak była.
+        return lane;
+      });
+
+    case DELETE_NOTE:
+      return state.map(lane => {
+        if (lane.id === action.laneId) {
+          const notes = [...lane.notes];
+          return notes.filter((note) => note.id !== action.noteId);
+        }
+        return lane;
+      });
+
 
     default:
       return state;
