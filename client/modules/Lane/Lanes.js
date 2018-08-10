6a880bc6afac3c0dbf4f3d3bc14a6436e38ef1a0
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-08-11 01:15:20 +0200

    Fix the bug in Lanes.js

--- client/modules/Lane/Lanes.js
+++ client/modules/Lane/Lanes.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import Lane from './LaneContainer.js';
 
+import styles from './Lane.css';
 //mapowaniem tablicy lanes przekazanej przez propsy na komponenty Lane
 //rozdzielenie danych o liniach z obiektu lanes na komponenty Lane za pomocą metody .map()
 //podczas iteracji po wielu elementach należy wykorzystać props key,(optymalizacja proc. renderujacych)
 const Lanes = ({ lanes }) => {
   return (
-    <div className="lanes">{lanes.map(lane =>
+    <div className={styles.Lanes}>{lanes.map(lane =>
         <Lane className="lane" key={lane.id} lane={lane} />
     )}</div>
   );
